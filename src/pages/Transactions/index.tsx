61e5893a6993d6da983cc15282697513f7032ebc
Delson Sousa <delsonsousa@live.com>
2023-01-17 23:40:47 -0300

    first commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,41 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 17.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>14/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 17,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>14/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
+}
